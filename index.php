f6207537a8b85c568a19c2d32ca32fc318a734a5
Giovanni <giovidoh@gmail.com>
2023-02-02 23:01:40 +0000

    Implémentation du Hero de la page d'accueil.

--- index.php
+++ index.php
@@ -10,19 +10,44 @@
 
 <!-- MAIN -->
 <main>
-    <div class="heading">
-        <div class="slider">
-            <div class="slides">
-                <div class="slide">
-                    <img src="" alt="">
+    <div class="main-hero">
+        <div class="main-hero__slider">
+            <div class="main-hero__slides">
+                <div class="main-hero__slides__slide">
+                    <div class="main-hero__text">
+                        <h2 class="main-hero__heading"> Bienvenue! </h2>
+                        <p class="main-hero__intro">
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat eius fugiat tempore. Dolore ut officia vitae qui? Maxime sapiente assumenda quidem ratione temporibus perferendis neque blanditiis dolorem laboriosam! Eos, neque.
+                        </p>
+                    </div>
                 </div>
-                <div class="slide">
-                    <img src="" alt="">
+                <div class="main-hero__slides__slide">
+                    <div class="main-hero__text">
+                        <h2 class="main-hero__heading"> Bienvenue! </h2>
+                        <p class="main-hero__intro">
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat eius fugiat tempore. Dolore ut officia vitae qui? Maxime sapiente assumenda quidem ratione temporibus perferendis neque blanditiis dolorem laboriosam! Eos, neque.
+                        </p>
+                    </div>
                 </div>
-                <div class="slide">
-                    <img src="" alt="">
+                <div class="main-hero__slides__slide">
+                    <div class="main-hero__text">
+                        <h2 class="main-hero__heading"> Bienvenue! </h2>
+                        <p class="main-hero__intro">
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat eius fugiat tempore. Dolore ut officia vitae qui? Maxime sapiente assumenda quidem ratione temporibus perferendis neque blanditiis dolorem laboriosam! Eos, neque.
+                        </p>
+                    </div>
+                </div>
+                <div class="main-hero__slides__slide">
+                    <div class="main-hero__text">
+                        <h2 class="main-hero__heading"> Bienvenue! </h2>
+                        <p class="main-hero__intro">
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat eius fugiat tempore. Dolore ut officia vitae qui? Maxime sapiente assumenda quidem ratione temporibus perferendis neque blanditiis dolorem laboriosam! Eos, neque.
+                        </p>
+                    </div>
                 </div>
             </div>
+        </div>
+        <div class="main-hero__text">
         </div>
     </div>
 </main>
