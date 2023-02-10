b9c47f1ac9c10211356833adc5109a487c71d56b
Giovanni <giovidoh@gmail.com>
2023-02-10 18:37:22 +0000

    Grosse MAJ page d'accueil.

--- index.php
+++ index.php
@@ -15,33 +15,33 @@
             <div class="main-hero__slides">
                 <div class="main-hero__slides__slide">
                     <div class="main-hero__text">
-                        <h2 class="main-hero__heading"> Bienvenue! </h2>
+                        <h2 class="main-hero__heading"></h2>
                         <p class="main-hero__intro">
-                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat eius fugiat tempore. Dolore ut officia vitae qui? Maxime sapiente assumenda quidem ratione temporibus perferendis neque blanditiis dolorem laboriosam! Eos, neque.
+                            Osez Mr. Shaker Bar Mobile Service, ce bar à cocktails atypiques qui se déplace jusqu'à votre lieu d'événement.
                         </p>
                     </div>
                 </div>
                 <div class="main-hero__slides__slide">
                     <div class="main-hero__text">
-                        <h2 class="main-hero__heading"> Bienvenue! </h2>
+                        <h2 class="main-hero__heading"></h2>
                         <p class="main-hero__intro">
-                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat eius fugiat tempore. Dolore ut officia vitae qui? Maxime sapiente assumenda quidem ratione temporibus perferendis neque blanditiis dolorem laboriosam! Eos, neque.
+                            En choisissant <b>Mr. Shaker Bar Mobile Service</b>, c'est la garantie d'un événement unique en son genre avec la participation d'un mixologue professionnel, Mr Shaker, qui sera à votre écoute afin de réaliser les cocktails et les boissons qui vous ressemblent.
                         </p>
                     </div>
                 </div>
                 <div class="main-hero__slides__slide">
                     <div class="main-hero__text">
-                        <h2 class="main-hero__heading"> Bienvenue! </h2>
+                        <h2 class="main-hero__heading"></h2>
                         <p class="main-hero__intro">
-                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat eius fugiat tempore. Dolore ut officia vitae qui? Maxime sapiente assumenda quidem ratione temporibus perferendis neque blanditiis dolorem laboriosam! Eos, neque.
+                            <b class="third">Mr. Shaker Bar Mobile Service</b> s'adapte à votre envie et anime tout type d'événements particuliers et professionnels, allant de 10 à 500 personnes.
                         </p>
                     </div>
                 </div>
                 <div class="main-hero__slides__slide">
                     <div class="main-hero__text">
-                        <h2 class="main-hero__heading"> Bienvenue! </h2>
+                        <h2 class="main-hero__heading"></h2>
                         <p class="main-hero__intro">
-                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat eius fugiat tempore. Dolore ut officia vitae qui? Maxime sapiente assumenda quidem ratione temporibus perferendis neque blanditiis dolorem laboriosam! Eos, neque.
+                            Osez <b>Mr. Shaker Bar Mobile Service</b>, ce bar à cocktails atypiques qui se déplace jusqu'à votre lieu d'événement.
                         </p>
                     </div>
                 </div>
@@ -50,7 +50,42 @@
     </div>
 
     <div class="main-body">
-        <button class="plus-button"><img src="images/plus.png" alt="Icone plus"></button>
+
+        <section class="main-body__section-one">
+            <div class="main-body__section-one__text">
+                <h2 class="main-body__section-one__text__heading">Bienvenue chez <br> Mr Shaker !</h2>
+                <p class="main-body__section-one__text__desc">
+                    Mr. Shaker Bar Mobile Service est une structure togolaise spécialisée en service de bars mobiles sur mesure et haute gamme pour tout événement particulier ou professionnel.
+                    <br><br>
+                    En choisissant Mr. Shaker Bar Mobile Service, c'est la garantie d'un événement unique en son genre avec la participation d'un mixologue professionnel, Mr Shaker, qui sera à votre écoute afin de réaliser les cocktails et les boissons qui vous ressemblent.
+                </p>
+            </div>
+            <div class="main-body__section-one__image">
+                <div class="main-body__section-one__image__one"></div>
+                <div class="main-body__section-one__image__two"></div>
+            </div>
+        </section>
+
+        <section class="main-body__section-two">
+            <button class="plus-button"><img src="images/plus.png" alt="Icone plus"></button>
+            <button class="plus-button"><img src="images/plus.png" alt="Icone plus"></button>
+            <button class="plus-button"><img src="images/plus.png" alt="Icone plus"></button>
+            <button class="plus-button"><img src="images/plus.png" alt="Icone plus"></button>
+            <button class="plus-button"><img src="images/plus.png" alt="Icone plus"></button>
+            <button class="plus-button"><img src="images/plus.png" alt="Icone plus"></button>
+        </section>
+
+        <section class="main-body__section-three">
+
+        </section>
+
+        <section class="main-body__section-four">
+
+        </section>
+
+        <section class="main-body__section-five">
+
+        </section>
     </div>
 </main>
 
--- php-partials/footer.php
+++ php-partials/footer.php
@@ -1,19 +1,34 @@
     <footer>
         <div class="footer-infos">
             <div class="footer-infos__item one">
-                <p class="footer-infos__headings">About us</p>
+                <p class="footer-infos__item__heading">About us</p>
+                <div class="footer-infos__item__div email">
+                    <img class="footer-infos__item__AboutUs-icon" src="images/mail.png" alt="">
+                    hello@gmail.com
+                </div>
+                <div class="footer-infos__item__div telephone">
+                    <img class="footer-infos__item__AboutUs-icon" src="images/telephone.png" alt="">
+                    +228 12345678
+                </div>
+                <div class="footer-infos__item__div adresse">
+                    <img class="footer-infos__item__AboutUs-icon" src="images/adresse.png" alt="">
+                    Lomé
+                </div>
+                
+                
             </div>
     
             <div class="footer-infos__item two">
-                <p class="footer-infos__headings">For business</p>
+                <p class="footer-infos__item__heading">For business</p>
+                
             </div>
 
             <div class="footer-infos__item three">
-                <p class="footer-infos__headings">Newsletter</p>
+                <p class="footer-infos__item__heading">Newsletter</p>
             </div>
 
             <div class="footer-infos__item four">
-                <p class="footer-infos__headings">Follow us</p>
+                <p class="footer-infos__item__heading">Follow us</p>
             </div>
 
         </div>
